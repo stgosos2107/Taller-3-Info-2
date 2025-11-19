0a8e876bdeeca0b9f60bf48985511f06023d36ea
sofia.hosorio <sofia.hosorio@udea.edu.co>
2025-11-19 18:50:07 -0500

    primera parte de la clase

--- procesador_dicom.py
+++ procesador_dicom.py
@@ -9,3 +9,20 @@ import os
 import numpy as np
 import pandas as pd
 import pydicom
+
+class ProcesadorDICOM:
+    COLUMNA_INTENSIDAD = "IntensidadPromdio"  # Se deja igual que en el enunciado del taller
+
+    def _init_(self, carpeta):
+        # Carpeta donde están los DICOM
+        self.carpeta = carpeta
+        self.datasets = []      # Aquí se guardan los DICOM leídos
+        self.rutas_archivos = []  # Ruta de cada archivo leído
+        self.df = pd.DataFrame()  # DataFrame final con metadatos
+
+#Este método recorre la carpeta y trata de leer todos los DICOM
+    def cargar_archivos(self):
+        if not os.path.isdir(self.carpeta):
+            print("La ruta no es un directorio:", self.carpeta)
+            return
+        
