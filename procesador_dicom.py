cc622b2d39da42c9e6bc9e6e65ddb7851184ced3
sofia.hosorio <sofia.hosorio@udea.edu.co>
2025-11-19 18:46:09 -0500

    inicio

---
+++ procesador_dicom.py
@@ -0,0 +1,11 @@
+#Taller Informática 2 Unidad 3
+
+#Creación de programa para leer DICOM, sacar metadatos y calcular la intensidad 
+# promedio de cada imagen.
+
+
+#Importaciones necesarias
+import os
+import numpy as np
+import pandas as pd
+import pydicom
